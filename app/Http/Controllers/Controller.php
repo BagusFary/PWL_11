df4e56db3b2d0655c4ff98bd09db4a1a5ea55455
BagusFary <bagusfaryananta@gmail.com>
2023-11-28 10:56:04 +0700

    Update

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,4 +9,19 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+    public function apiSuccess($data, $code = 200, $message = null)
+    {
+        return response()->json([
+            'data' => $data,
+            'message' => $message,
+        ], $code);
+    }
+
+    public function apiError($errors, $code, $message = null)
+    {
+        return response()->json([
+            'errors' => $errors,
+            'message' => $message,
+        ], $code);
+    }
 }
